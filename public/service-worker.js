39f2168937f2f733dc7ab99e1bda94ea89243ac0
Daniel Lewis <daniel.lewis@protonmail.com>
2021-07-03 21:56:45 -0600

    removed 144x144.png

--- public/service-worker.js
+++ public/service-worker.js
@@ -9,7 +9,6 @@ const FILES_TO_CACHE = [
   "./icons/icon-72x72.png",
   "./icons/icon-96x96.png",
   "./icons/icon-128x128.png",
-  "./icons/icon-144x144.png",
   "./icons/icon-192x192.png",
   "./icons/icon-384x384.png",
   "./icons/icon-512x512.png",
